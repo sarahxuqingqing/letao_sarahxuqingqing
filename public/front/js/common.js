6ebef2b34afcef188ecd977d4feedc553666abec
sarahxuqingqing <409995016@qq.com>
2018-03-05 22:01:54 +0800

    手机端主页写完

--- public/front/js/common.js
+++ public/front/js/common.js
@@ -1,3 +1,9 @@
 /**
  * Created by xqq on 2018/3/5.
  */
+$(function(){
+    //初始化scroll插件
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
+})
